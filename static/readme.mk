ec1f786e0307bbb49a0bbf5963534005315320be
yndlsds <yndlsds@126.com>
2013-04-10 10:34:33 +0800

    'create'

---
+++ static/readme.mk
@@ -0,0 +1,7 @@
+folder: static
+used for: prepare html\css\js static files for tijihe
+
+2013.4.10 @gerry
+pull to github 
+css framework: metro-bootstrap generated using LESS
+icons: use icons.css; or use alternatively http://fontello.com/ instead later
